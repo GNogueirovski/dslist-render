31a855944a5351bc46809fa7c9b662e7c9dbdd34
Gabriel N <spbiel18@gmail.com>
2025-01-21 19:55:01 -0300

    Criação Repository

---
+++ src/main/java/com/nogueira/dslist/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.nogueira.dslist.repositories;
+
+import com.nogueira.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository  extends JpaRepository<Game, Long> {
+}
